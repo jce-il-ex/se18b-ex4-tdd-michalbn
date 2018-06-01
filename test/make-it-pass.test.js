3d7a20b94b57a1c1033fac3855a775ef692441ab
Michal <michal1369@hotmail.com>
2018-06-01 04:46:15 +0300

    GREEN: add more test to make-it-pass.test and pass

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -5,7 +5,17 @@ function sum (a, b) {
   return a+b;
 }
 
+function multiply (a, b) {
+  // your code to make the test pass goes here ...
+  return a*b;
+}
+
 test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.end();
+});
+
+test('sum should return the addition of two numbers', function (t) {
+  t.equal(30, multiply(5, 6)); // make this test pass by completing the add function!
   t.end();
 });
